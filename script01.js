a4a0f2b0939a8b1a4a646e24310f593f9f5fe97c
Taynara Sauner <taynara.5996@aluno.pr.senac.br>
2024-10-01 21:49:02 -0300

    Ativdade 27 ajustes

--- script01.js
+++ script01.js
@@ -1,27 +1,25 @@
 // Mostrar o  nome do mês com base no número do mês
 
 const nomeMeses = [
-    "Janeiro",
-    "Fevereiro",
-    "Março",
-    "Abril",
-    "Junho",
-    "Julho",
-    "Agosto",
-    "Setembro",
-    "Outubro",
-    "Novembro",
-    "Dezembro"
+    'Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'
 ]
 
-const mes = Number(prompt("Informe o número do mês"))
+const mes = prompt("Informe o número do mês")
+const numeroMes = Number(mes)
+let valorMes
+let mensagem = "Mês Inválido"
 
-if (mes > 0) {
-    console.log(nomeMeses[mes - 1])
+if ((numeroMes != NaN) && (numeroMes > 0) && (numeroMes <= 12)) {
 
+    mensagem = `${nomeMeses[numeroMes - 1]} é o mês de número ${numeroMes}`
 }
 else {
-    console.log("Mes inválido")
+    for (let index = 0; index < nomeMeses.length; index++) {
+        if (mes.toLowerCase() === nomeMeses[index].toLowerCase()) {
+            mensagem = `${mes} è o mês de número ${index + 1}`
+            break
+        }
+    }
 }
 
-
+console.log(mensagem)
